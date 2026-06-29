ec230219a558f9183776351022ac4d42f25d21e1
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-06-29 09:28:49 +0600

    Add Watchlist API

---
+++ src/controllers/watchlistController.js
@@ -0,0 +1,47 @@
+import { prisma } from "../config/db.js";
+
+const addToWatchlist = async (req, res) => {
+  const { movieId, status, rating, notes, userId } = req.body;
+
+  // Verify whether the movie exists or not
+  const movie = await prisma.movie.findUnique({
+    where: { id: movieId },
+  });
+
+  if (!movie) {
+    return res.status(404).json({ error: "Movie Not Found!" });
+  }
+
+  // Verify whether movie is already added to watchlist or not
+  const existingInWatchlist = await prisma.watchlistItem.findUnique({
+    where: {
+      userId_movieId: {
+        userId: userId,
+        movieId: movieId,
+      },
+    },
+  });
+
+  if (existingInWatchlist) {
+    return res.status(400).json({ error: "Movie already in the watchlist" });
+  }
+
+  const watchlistItem = await prisma.watchlistItem.create({
+    data: {
+      userId,
+      movieId,
+      status: status || "PLANNED",
+      rating,
+      notes,
+    },
+  });
+
+  res.status(201).json({
+    status: "Success",
+    data: {
+      watchlistItem,
+    },
+  });
+};
+
+export { addToWatchlist };
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import { connectDB, disconnectDB } from "./config/db.js";
 import movieRoutes from "./routes/movieRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import watchlistRoutes from "./routes/watchlistRoutes.js";
 
 config();
 connectDB();
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // API Routes
 app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
+app.use("/watchlist", watchlistRoutes);
 
 const PORT = 5001;
 const server = app.listen(PORT, () => {
